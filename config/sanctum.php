996444ecd315e0ce095140976807affca54bedea
Tentchou nana romeo <tentchouromeo58@gmail.com>
2025-05-28 00:59:15 +0100

    romeo procfile

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000,127.0.0.1:3000,todoproject-ten.vercel.app')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000,127.0.0.1:3000,https://todoproject-ten.vercel.app')),
 
 
 
